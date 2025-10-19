e1f09fa34bc566ba9a27a40e8e7242c0f1fb0dc9
DawidKaczy <kaczorek2002@wp.pl>
2025-10-19 16:37:34 +0200

    Dodano nowe funkcje

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
-from .models import Category, Topic
+from .models import Category, Topic, Post
 
-admin.site.register(Category)
-admin.site.register(Topic)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'created')
+    list_filter = ('name', 'category', 'created')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('created_at',)
+
+    list_display = ('title', 'short_text_preview', 'topic', 'slug', 'created_at', 'updated_at', 'created_by')
+    list_filter = ('topic', 'created_by')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+    def short_text_preview(self, obj):
+        words = obj.text.split()
+        if len(words) > 5:
+            return ' '.join(words[:5]) + '...'
+        else:
+            return obj.text
+    short_text_preview.short_description = 'Początek tekstu'
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,11 +1,40 @@
 from django.db import models
+from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.name
 
+    class Meta:
+        ordering = ['name']
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+    slug = models.SlugField(default="", null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+
+
